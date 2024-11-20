1c3f4760921ff407d12d6417eef028a48944c79a
Nelson Durrant <snelsondurrant@gmail.com>
2024-11-19 18:12:19 -0700

    Quick fixes

--- init.sh
+++ init.sh
@@ -4,22 +4,19 @@
 # Calls ROS services to start the FSMs
 # - Specify a task configuration using 'bash init.sh <task>' (ex. 'bash init.sh sort')
 
-export PARAM_FILE=/home/agrobot/config/vehicle_params.yaml
-
 source ~/ros2_ws/install/setup.bash
-
 case $1 in
     "sort")
-        ros2 service call /sort/start agrobot_interfaces/srv/StartFSM param_file:=$PARAM_FILE
+        ros2 service call /sort/start agrobot_interfaces/srv/StartFSM
         ;;
     "navigate")
-        ros2 service call /navigate/start agrobot_interfaces/srv/StartFSM param_file:=$PARAM_FILE
+        ros2 service call /navigate/start agrobot_interfaces/srv/StartFSM
         ;;
     "handle")
-        ros2 service call /handle/start agrobot_interfaces/srv/StartFSM param_file:=$PARAM_FILE
+        ros2 service call /handle/start agrobot_interfaces/srv/StartFSM
         ;;
     "collect")
-        ros2 service call /collect/start agrobot_interfaces/srv/StartFSM param_file:=$PARAM_FILE
+        ros2 service call /collect/start agrobot_interfaces/srv/StartFSM
         ;;
     *)
         printError "No task configuration specified"
--- launch.sh
+++ launch.sh
@@ -4,6 +4,8 @@
 # Starts the micro-ROS agent and ROS 2 launch files
 # - Specify a task configuration using 'bash launch.sh <task>' (ex. 'bash launch.sh sort')
 
+export PARAM_FILE=/home/agrobot/config/vehicle_params.yaml
+
 function printInfo {
   echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
@@ -23,26 +25,26 @@ echo "################################################################"
 echo ""
 
 # Test for Teensy board connection
-if [ -z "$(tycmd list | grep Teensy)" ]; then
-    printError "No Teensy boards avaliable to connect to"
-    exit 1
-fi
+# if [ -z "$(tycmd list | grep Teensy)" ]; then
+#     printError "No Teensy boards avaliable to connect to"
+#     exit 1
+# fi
 
 # Start both workspaces
 source ~/microros_ws/install/setup.bash
 source ~/ros2_ws/install/setup.bash
 case $1 in
     "sort")
-        ros2 launch agrobot sort_launch.py
+        ros2 launch task_fsm sort_launch.py param_file:=$PARAM_FILE
         ;;
     "navigate")
-        ros2 launch agrobot navigate_launch.py
+        ros2 launch task_fsm navigate_launch.py param_file:=$PARAM_FILE
         ;;
     "handle")
-        ros2 launch agrobot handle_launch.py
+        ros2 launch task_fsm handle_launch.py param_file:=$PARAM_FILE
         ;;
     "collect")
-        ros2 launch agrobot collect_launch.py
+        ros2 launch task_fsm collect_launch.py param_file:=$PARAM_FILE
         ;;
     *)
         printError "No task configuration specified"
